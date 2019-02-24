c98c3cc38f1760b602ab9713f6c6862c897949e5
Felice Forby <feliceforby@gmail.com>
2019-02-24 22:41:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ function forkRepo() {
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  const resultsDiv = document.getElementById(results);
+  resultsDiv.innerHTML = `<a href="${json.html_url}" target="_blank">${json.html_url}</a>`;
 }
 
 function createIssue() {
