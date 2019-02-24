c65236129778fe6a74843eddd348eaf4b78676cd
Felice Forby <feliceforby@gmail.com>
2019-02-24 22:34:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+const rootURL = "https://api.github.com";
+const username ="morinoko";
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  return '135b3c049957c94dced1d7b283705a6cb525aed8';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  fetch(
+    'https://api.github.com/repos/learn-co-curriculum/js-ajax-fetch-lab/forks',
+    {
+      method: 'POST',
+      heaaders: {
+        Authorization: `token ${getToken()}`
+      }
+    }
+  ).then(response => console.log(response));
 }
 
 function showResults(json) {
